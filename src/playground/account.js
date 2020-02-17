cb6ce499da19ba6d8247e4af404d451e0a6641ff
ech3640 <edward_c_holmes@homedepot.com>
2020-02-17 09:28:26 -0500

    corrected issue that was preventing state from updating on screen

--- src/playground/account.js
+++ src/playground/account.js
@@ -8,25 +8,13 @@ class Account extends React.Component {
     }
     addToBalance(e) {
         e.preventDefault()
-        
-        console.log(this.state.balance, 'state')
 
-        const depositAmount = e.target.elements.depositAmount.value
+        const depositAmount = parseInt(e.target.elements.depositAmount.value, 10)
         e.target.elements.depositAmount.value = ''
 
-        console.log('deposit amount', Number(depositAmount))
-        
-        const newBalance = Number(depositAmount + this.state.balance)
-
-        console.log('new bal', Number(newBalance))
-
-        this.setState((prevState) => {console.log(typeof prevState.balance)
-            
-            { balance: prevState.balance + depositAmount }})
-
-        // this.setState((prevState) => ({ count: prevState.count + 1 }))
-        
-
+        this.setState((prevState) => {
+            return { balance: prevState.balance + depositAmount }
+        })
     }
 
     render() {
