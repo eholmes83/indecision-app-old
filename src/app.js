1c486b005fc5b903d0c0c9ac3318dafef7747dfc
ech3640 <edward_c_holmes@homedepot.com>
2020-02-18 17:00:46 -0500

    file being compliled with babel and webpack

--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
-// import './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// import subtract, { square, add } from './utils'
-
-// console.log('Running from app.js')
-// console.log(square(4), 'squared')
-// console.log(add(4,8), 'add')
-// console.log(subtract(21,14))
-
-
-import isSenior, { isAdult, canDrink } from './person'
-
-console.log('is adult?', isAdult(25))
-console.log('can drink?', canDrink(50))
-console.log('is senior?', isSenior(64))
+const template = <p>JSX from webpack</p>
+ReactDOM.render(template, document.getElementById('app'))
