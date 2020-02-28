0eb5dd0b3287da1f1475dd3808346127d2e87d30
ech3640 <edward_c_holmes@homedepot.com>
2020-02-28 09:02:29 -0500

    added classnames for styling purposes

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,19 +3,24 @@ import Option from './Option'
 
 const Options = (props) => (
     <div>
-      <button 
-        onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option!</p>}
+      <div className='widget-header'>
+        <h3 className='widget-header__title'>Your Options</h3>
+        <button 
+          onClick={props.handleDeleteOptions}
+          className='button button--link'
+          >Remove All</button>
+    </div>
+        {props.options.length === 0 && <p className='widget__message'>Add an option to get started!</p>}
       {
-        props.options.map(option => 
+        props.options.map((option, index) => 
           <Option 
-            key={option} 
+            key={option}
+            count={index + 1}
             optionText={option}
             handleDeleteOption={props.handleDeleteOption}
           />)
       }
     </div>
   )
-
 
 export default Options
